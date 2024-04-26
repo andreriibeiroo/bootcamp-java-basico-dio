a09df86c873eb08759d5d001a5e8cfa50bae4820
andre <dekoribeiro2013@gmail.com>
2024-04-26 17:10:56 -0400

    exercio basico

--- Metodos exercicios/sistema-smart-tv/src/SmartTv.java
+++ Metodos exercicios/sistema-smart-tv/src/SmartTv.java
@@ -3,6 +3,7 @@ public class SmartTv {
     boolean ligada=false;
     int canal = 1;
     int volume = 25;
+    
 
     public void ligar(){
         ligada = true;
--- Metodos exercicios/sistema-smart-tv/src/Usuario.java
+++ Metodos exercicios/sistema-smart-tv/src/Usuario.java
@@ -20,7 +20,6 @@ public class Usuario {
     smartTv.aumentarCanal();
     smartTv.dimunuirCanal();
 
-
   
     }
 }
