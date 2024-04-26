41e74ae7eb1740ad517eee9b540c7088505fe284
andre <dekoribeiro2013@gmail.com>
2024-04-26 17:08:44 -0400

    alteracao no nome

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,11 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        System.out.println("teste");
+
+
+        int numero = -4;
+        System.out.println(- numero);
     }
+    
 }
